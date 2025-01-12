54efc0c4e6363a2cf30938f19ede8cdf3da47ec9
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-01-12 08:39:22 +0530

    Update DeQueue.java

--- DeQueue.java
+++ DeQueue.java
@@ -1,1 +1,25 @@
 //dequeue == delete elements from queue
+class QueueArray
+{
+  static int SIZE=10;
+  static int front=-1;
+  static int rear=-1;
+  static int array[]=new int[SIZE];
+}
+class Main{
+  public static void main(String args[]){
+    QueueArray ob1=new QueueArray();
+    ob1.enqueue(10);
+    ob1.enqueue(20);
+    ob1.enqueue(30);
+    ob1.enqueue(40);
+
+    ob1.dequeue();
+    ob1.dequeue();
+    ob1.dequeue();
+    ob1.dequeue();
+    ob1.dequeue();
+
+    ob1.enqueue(50);
+  }
+}
