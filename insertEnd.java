586b1995ebd1b96f9c4cbc8211d7d513f179adcf
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-01-14 17:43:37 +0530

    code insertEnd.java

--- insertEnd.java
+++ insertEnd.java
@@ -1,1 +1,47 @@
 
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class insertEnd {
+
+    static Node insertEnd(Node head, int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return head;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " - ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+        head = insertEnd(head, 10);
+        head = insertEnd(head, 20);
+        head = insertEnd(head, 30);
+        head = insertEnd(head, 40);
+        System.out.println("Linked List:");
+        printList(head);
+    }
+}
